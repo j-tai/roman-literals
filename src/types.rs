6fb5122873d302588fc5733609b50ea6f8ec3e07
James Tai <jtai@jtai.ca>
2022-08-26 14:39:11 -0700

    Initial commit

---
+++ src/types.rs
@@ -0,0 +1,23 @@
+#![allow(non_camel_case_types)]
+
+/// The VIII-bit signed integer type.
+pub type iVIII = i8;
+/// The XVI-bit signed integer type.
+pub type iXVI = i16;
+/// The XXXII-bit signed integer type.
+pub type iXXXII = i32;
+/// The LXIV-bit signed integer type.
+pub type iLXIV = i64;
+/// The CXXVIII-bit signed integer type.
+pub type iCXXVIII = i128;
+
+/// The VIII-bit unsigned integer type.
+pub type uVIII = u8;
+/// The XVI-bit unsigned integer type.
+pub type uXVI = u16;
+/// The XXXII-bit unsigned integer type.
+pub type uXXXII = u32;
+/// The LXIV-bit unsigned integer type.
+pub type uLXIV = u64;
+/// The CXXVIII-bit unsigned integer type.
+pub type uCXXVIII = u128;
